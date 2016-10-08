9a1698e802d16befaf1d3ae10a255c5511b1fb20
Codeship dcoy <david@obsidianspork.io>
2016-10-08 14:19:24 -0500

    Initial script commit

---
+++ homepage.rb
@@ -0,0 +1,34 @@
+# Simple HTTP client to retrive any page
+require 'net/http'
+
+# puts "Enter a URL/URI: "
+# userURI = gets.chomp
+
+# # Clarify input
+# puts "You just entered: #{userURI}"
+# Basic GET request
+
+uri = URI.parse('http://example.com/')
+# # Shortcut
+# response = Net::HTTP.get_response(uri)
+
+# # Will print response.body
+# Net::HTTP.get_print(uri)
+
+# Full
+http = Net::HTTP.new(uri.host, uri.port)
+request = Net::HTTP::Get.new(uri.request_uri)
+
+response = http.request(request)
+
+response.code # => 301
+response.body # => The body (HTML, XML, etc.)
+
+# Headers are lowercased
+response['cache-control'] # => public, max-age=2592000
+
+puts "Response code: #{response.code}" # response.code
+
+puts "Here is the response body:
+
+#{response.body}"
